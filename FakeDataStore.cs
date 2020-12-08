10b3c30b15f853a08dc259a382f9539ce71a6505
Juan Pablo <totomarion@gmail.com>
2020-12-08 10:54:39 -0300

    Crear GetValuesQuery

---
+++ FakeDataStore.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace ApiRestMediatR
+{
+    public class FakeDataStore
+    {
+        private static List<string> _values;
+
+        public FakeDataStore()
+        {
+            _values = new List<string>
+            {
+                "a",
+                "b",
+                "c"
+            };           
+        }
+
+        public void AddValue(string value)
+        {
+            _values.Add(value);
+        }
+
+        public IEnumerable<string> GetAllValues()
+        {
+            return _values;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -21,6 +21,7 @@ namespace ApiRestMediatR
         {
             services.AddControllers();
             services.AddMediatR(typeof(Startup));
+            services.AddSingleton<FakeDataStore>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
